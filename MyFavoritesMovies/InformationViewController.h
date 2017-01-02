c194221feedcc5de374b6b29da0457cee5d4e6ea
Lucas César <lucascesarnf@hotmail.com>
2017-01-02 11:06:02 -0200

    Information screen problem

---
+++ MyFavoritesMovies/InformationViewController.h
@@ -0,0 +1,13 @@
+//
+//  InformationViewController.h
+//  MyFavoritesMovies
+//
+//  Created by Lucas César  Nogueira Fonseca on 02/01/17.
+//  Copyright © 2017 Lucas César  Nogueira Fonseca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InformationViewController : UITableViewController
+
+@end
